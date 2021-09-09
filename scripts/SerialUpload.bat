7a4f3e9191fb5b997dbcd9899084fea76a5a343c
Daniel Wong <drojfjord@gmail.com>
2021-09-09 11:34:42 +1000

    remove pause from batch file

--- scripts/SerialUpload.bat
+++ scripts/SerialUpload.bat
@@ -1,4 +1,2 @@
 
-python AVR-DA_uploader.py "..\avr128da48-cnano-avr-da-host-app.X\dist\Release\production\AVR-Dx_App_Example.X.production.hex" 128000 COM11 9600
-pause
-
+python AVR-DA_uploader.py "..\avr128da48-cnano-avr-da-host-app.X\dist\Release\production\avr128da48-cnano-avr-da-host-app.X.production.hex" 128000 COM11 9600
